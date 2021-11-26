e500efb1eb32841dc32ef6b87ee6a8583e268313
drossano <72749793+drossano@users.noreply.github.com>
2021-11-26 16:31:25 -0500

    Add coordinates to gameboard

--- tictactoe.rb
+++ tictactoe.rb
@@ -5,12 +5,13 @@ class GameBoard
                   [" "," "," "],
                   [" "," "," "]]
 
-    @game_board = " #{game_array[0][0]} | #{game_array[0][1]} | #{game_array[0][2]} " + "\n" +
-                  "———————————" + "\n" +
-                  " #{game_array[1][0]} | #{game_array[1][1]} | #{game_array[1][2]} " + "\n" +
-                  "———————————" + "\n" +
-                  " #{game_array[2][0]} | #{game_array[2][1]} | #{game_array[2][2]} "
+    @game_board = "   A   B   C " + "\n" +
+                  "1  #{game_array[0][0]} | #{game_array[0][1]} | #{game_array[0][2]} " + "\n" +
+                  "  ———————————" + "\n" +
+                  "2  #{game_array[1][0]} | #{game_array[1][1]} | #{game_array[1][2]} " + "\n" +
+                  "  ———————————" + "\n" +
+                  "3  #{game_array[2][0]} | #{game_array[2][1]} | #{game_array[2][2]} "
   end
 end
 game = GameBoard.new
-puts game.game_board
+print game.game_board
