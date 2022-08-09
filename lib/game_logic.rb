52c08e6a71c40de803d620a7ac2a8a94a767b94f
Dean <72749793+drossano@users.noreply.github.com>
2022-08-08 21:45:14 -0400

    Add test to check for a #row_win and refactor how the board is passed to methods

--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -34,11 +34,11 @@ class GameLogic
     end
   end
 
-  def check_for_win_or_draw(symbol)
-    if [row_win(symbol), column_win(symbol), diagonal_win(symbol)].any? { |win_condition| win_condition == true }
+  def check_for_win_or_draw(symbol, board)
+    if [row_win(symbol, board), column_win(symbol, board), diagonal_win(symbol, board)].any? { |win_condition| win_condition == true }
       puts "Player #{symbol} wins!"
       true
-    elsif draw
+    elsif draw(board)
       puts "Draw!"
       true
     else
@@ -46,27 +46,27 @@ class GameLogic
     end
   end
 
-  def row_win(symbol)
-    @board.game_array.any? do |row|
+  def row_win(symbol, board)
+    board.any? do |row|
       row.all? { |space| space == symbol}
     end
   end
 
-  def column_win(symbol)
-    columns = @board.game_array.transpose
+  def column_win(symbol, board)
+    columns = board.transpose
     columns.any? do |column|
       column.all? { |space| space == symbol}
     end
   end
 
-  def diagonal_win(symbol)
-    diagonals = [[@board.game_array[0][0], @board.game_array[1][1], @board.game_array[2][2]], [@board.game_array[2][0], @board.game_array[1][1], @board.game_array[0][2]]]
+  def diagonal_win(symbol, board)
+    diagonals = [[board[0][0], board[1][1], board[2][2]], [board[2][0], board[1][1], board[0][2]]]
     diagonals.any? do |diagonal|
       diagonal.all? { |space| space == symbol }
     end
   end
 
-  def draw
-    @board.game_array.flatten.all? { |space| space != " "}
+  def draw(board)
+    board.flatten.all? { |space| space != " "}
   end
 end
--- spec/game_logic_spec.rb
+++ spec/game_logic_spec.rb
@@ -1,40 +1,63 @@
 require_relative '../lib/game_logic'
+require_relative '../lib/game_board'
 
 describe GameLogic do
   describe '#check_for_win_or_draw' do
-    subject (:win_check) { described_class.new }
+    subject(:win_check) { described_class.new }
     context 'when a row wins' do
       it 'returns true' do
         symbol = 'x'
+        board = [[], [], []]
         allow(win_check).to receive(:row_win).and_return(true)
-        expect(win_check.check_for_win_or_draw(symbol)).to eq(true)
+        expect(win_check.check_for_win_or_draw(symbol, board)).to eq(true)
       end
     end
     context 'when a column wins' do
       it 'returns true' do
         symbol = 'x'
+        board = [[], [], []]
         allow(win_check).to receive(:column_win).and_return(true)
-        expect(win_check.check_for_win_or_draw(symbol)).to eq(true)
+        expect(win_check.check_for_win_or_draw(symbol, board)).to eq(true)
       end
     end
     context 'when a diagonal wins' do
       it 'returns true' do
         symbol = 'x'
+        board = [[], [], []]
         allow(win_check).to receive(:diagonal_win).and_return(true)
-        expect(win_check.check_for_win_or_draw(symbol)).to eq(true)
+        expect(win_check.check_for_win_or_draw(symbol, board)).to eq(true)
       end
     end
     context 'when theres a draw' do
       it 'returns true' do
         symbol = 'x'
+        board = [[], [], []]
         allow(win_check).to receive(:draw).and_return(true)
-        expect(win_check.check_for_win_or_draw(symbol)).to eq(true)
+        expect(win_check.check_for_win_or_draw(symbol, board)).to eq(true)
       end
     end
     context 'when there isnt a game over condition' do
       it 'returns false' do
         symbol = 'x'
-        expect(win_check.check_for_win_or_draw(symbol)).to eq(false)
+        board = [[], [], []]
+        allow(win_check).to receive(:row_win).and_return(false)
+        allow(win_check).to receive(:column_win).and_return(false)
+        allow(win_check).to receive(:diagonal_win).and_return(false)
+        allow(win_check).to receive(:draw).and_return(false)
+        expect(win_check.check_for_win_or_draw(symbol, board)).to eq(false)
+      end
+    end
+  end
+
+  describe '#row_win' do
+    context 'when theres a win in the top row' do
+      let(:top_row) { double('game')}
+      it 'returns true' do
+        symbol = "X"
+        board = [[symbol, symbol, symbol],
+        [" ", " ", " "],
+        [" ", " ", " "]]
+        expect(subject.row_win(symbol, board)).to eq(true)
       end
     end
   end
